3aed244e028b8c384420a71522ee01be63c5e273
zer0131 <zer0131@vip.qq.com>
2016-04-06 16:04:41 +0800

    check code接口开发完成

--- service/Controller/Sso/ApiController.php
+++ service/Controller/Sso/ApiController.php
@@ -8,6 +8,9 @@ namespace Controller\Sso;
 
 use OneFox\ApiController as BaseController;
 use OneFox\Request;
+use Lib\SSO\Code;
+use Lib\SSO\Session;
+use Lib\SSO\Ticket;
 
 class ApiController extends BaseController {
     
@@ -27,6 +30,23 @@ class ApiController extends BaseController {
         if (!$code || !$appKey || !$appId) {
             $this->json(self::CODE_FAIL, 'params error');
         }
+        $codeObj = new Code();
+        $res = $codeObj->checkCode($code);//返回session id
+        if (empty($res)) {
+            $this->json(self::CODE_FAIL, 'check code error');
+        }
+
+        //获取session
+        $sessObj = new Session(); 
+        $sessionData = $sessObj->getSession($res);
+
+        //生成子系统ticket
+        $ticketObj = new Ticket();
+        $ticket = $ticketObj->genSubTicket($res);
+
+        //返回数据
+        $data = array('ticket'=>$ticket, 'username'=>$sessionData['username']);
+        $this->json(self::CODE_SUCCESS, 'ok', $data);
     }
 
 
