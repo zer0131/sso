79f74d89de122785309f9aa9e8879ab0f877cbdc
zer0131 <zer0131@vip.qq.com>
2016-04-06 16:02:42 +0800

    ticket操作类完成

--- service/Lib/SSO/Ticket.php
+++ service/Lib/SSO/Ticket.php
@@ -7,6 +7,49 @@
 
 namespace Lib\SSO;
 
+use OneFox\Crypt;
+
 class Ticket {
-    //code
+    const PREFIX = 'sso_ticket:';
+
+    /**
+     * 生成子系统ticket
+     * @param string $str
+     * @param string $prefix
+     * @return string
+     */
+    public function genSubTicket($str, $prefix=self::PREFIX){
+        return Crypt::encode($prefix.$str);
+    }
+
+    /**
+     * 生成服务端ticket
+     * @param string $str
+     * @param string $prefix
+     * @return string
+     */
+    public function genSysTicket($str, $prefix=self::PREFIX){
+        return Crypt::encode($prefix.$str);
+    }
+
+    /**
+     * 依据ticket返回原始值
+     * @param string $ticket
+     * @param string $prefix
+     * @return string
+     */
+    public function originData($ticket, $prefix=self::PREFIX){
+        $decodeStr = Crypt::decode($ticket);
+        return substr($decodeStr, strlen($prefix));
+    }
+
+    /**
+     * 校验ticket
+     * @param string $encodedStr
+     */
+    public function checkTicket($ticket){
+        $sessionId = $this->originData($ticket);
+        $sessObj = new Session();
+        return $sessObj->isExists($sessionId);//使用session校验
+    }
 }
