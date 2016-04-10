42e8a7c8aa40856acfb9c5010c34f840483c7c78
zer0131 <zer0131@vip.qq.com>
2016-04-10 21:14:08 +0800

    添加sso logout控制器

---
+++ service/Controller/Sso/LogoutController.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @author ryan
+ * @desc sso退出控制器
+ */ 
+
+namespace Controller\Sso;
+
+use OneFox\ApiController;
+use OneFox\Config;
+use OneFox\Request;
+use Lib\SSO\Session;
+use Lib\SSO\Ticket;
+use OneFox\Response;
+
+class LogoutController extends ApiController {
+
+    public function indexAction() {
+        $app_id = $this->get('app_id', 0, 'int');
+        if (!$app_id) {
+            echo 'params error';
+            return;
+        }
+        $jumpto = $this->get('jumpto');
+
+        $cookieName = md5(Config::get('sso.cookie_name'));
+        $cookie = Request::cookie($cookieName);
+
+        $ticketObj = new Ticket();
+        $sessObj = new Session();
+        $sessionId = $ticketObj->originData($cookie);
+
+        //清除session
+        $sessObj->delete($sessionId);
+
+        //清除服务端cookie
+        setcookie($cookieName, '', time()-31500000, Config::get('sso.cookie_path'), '');
+        $paramArr = array(
+            'jumpto' => $jumpto ? urlencode($jumpto) : 'index',
+            'app_id' => $appId
+        );
+        $param = http_build_query($paramArr);
+
+        Response::redirect(Config::get('sso.login_url').'?'.$param);
+    }
+}
